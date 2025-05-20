ee5c536fbc668fcb7006c3debb9350a92701e2b4
lensonlovescode <lensonisinparis@gmail.com>
2025-05-20 16:16:23 +0300

    More checker problems

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -3,6 +3,7 @@
 Flask Babel Simple Recall of how a flask app works
 """
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
@@ -11,8 +12,8 @@ def hello() -> str:
     """
     Simple flask recall of how a flask app runs
     """
-    return (render_template("0-index.html"))
+    return render_template("0-index.html")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
