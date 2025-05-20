6b51d787fb1f1968832a72fccf51ad6a50d9e7d4
lensonlovescode <lensonisinparis@gmail.com>
2025-05-20 16:12:14 +0300

    Checker problems

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 """
-Flask Babel
+Flask Babel Simple Recall of how a flask app works
 """
 from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/', methods=["GET"], strict_slashes=False)
 def hello() -> str:
     """
-    Simple flask recall
+    Simple flask recall of how a flask app runs
     """
-    return (render_template('0-index.html'))
+    return (render_template("0-index.html"))
 
 
 if __name__ == '__main__':
