1970b33cdc9c61c750c28fa1148a89b4e1822c46
lensonlovescode <lensonisinparis@gmail.com>
2025-05-21 09:52:48 +0300

    Accepted languages

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Babel instanciation of a flask app
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config():
+    """
+    A config class to define the languages supported or sum
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_TIMEZONE = ["UTC"]
+    BABEL_DEFAULT_LOCALE = 'en'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.locale_selector
+def get_locale():
+    """
+    Get locale function to fetch supported languages
+    """
+    return request.accepted_languages.best_match(Config.LANGUAGES)
+
+
+@app.route('/')
+def hello() -> str:
+    """
+    Renders the hello world template
+    """
+    return render_template("1-index.html",)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
