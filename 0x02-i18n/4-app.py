503dfba9e1d236f3e86117dfea6fe2cd744ed2e2
lensonlovescode <lensonisinparis@gmail.com>
2025-05-23 13:39:58 +0300

    Correct code

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -19,7 +19,10 @@ app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
 
+babel = Babel(app)
 
+
+@babel.localeselector
 def get_locale():
     """
     Get locale function to fetch supported languages
@@ -29,9 +32,6 @@ def get_locale():
         return locale
 
     return request.accept_languages.best_match(app.config["LANGUAGES"])
-
-
-babel = Babel(app, locale_selector=get_locale)
 
 
 @app.route('/')
