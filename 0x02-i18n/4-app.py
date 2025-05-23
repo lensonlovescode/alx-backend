fb8e9e3c4b7d708d3d2cfb29921ed2554adbc2f8
lensonlovescode <lensonisinparis@gmail.com>
2025-05-23 13:18:08 +0300

    Locale not lang

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -27,7 +27,7 @@ def get_locale():
     """
     Get locale function to fetch supported languages
     """
-    return request.args.get('lang') or request.accept_languages.best_match(
+    return request.args.get('locale') or request.accept_languages.best_match(
            app.config["LANGUAGES"])
 
 
